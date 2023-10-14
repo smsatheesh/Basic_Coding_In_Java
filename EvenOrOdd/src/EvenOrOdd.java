d549c2dee9c2ac9849606348047e1fe0029604d4
smsatheesh <147731426+smsatheesh@users.noreply.github.com>
2023-10-14 13:06:25 +0530

    Update EvenOrOdd.java

--- EvenOrOdd/src/EvenOrOdd.java
+++ EvenOrOdd/src/EvenOrOdd.java
@@ -5,13 +5,13 @@ class EvenOrOdd {
 	static void checkType(int num) {
 		
 		if(num != 0) {
-    		if(num % 2 == 0)
-        		System.out.println(num + " is even number");
-        	else
-        		System.out.println(num + " is odd number");
-    	} else {
-    		System.out.println("Number is neither even nor odd");
-    	}
+	    		if(num % 2 == 0)
+	        		System.out.println(num + " is even number");
+	        	else
+	        		System.out.println(num + " is odd number");
+	    	} else {
+	    		System.out.println("Number is neither even nor odd");
+	    	}
 	}
 	
     public static void main(String[] args) {
