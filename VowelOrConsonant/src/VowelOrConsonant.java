728ab39eb4d5e47b0101773047d1a364fd16914f
smsatheesh <147731426+smsatheesh@users.noreply.github.com>
2023-10-15 14:23:44 +0530

    Update VowelOrConsonant.java

--- VowelOrConsonant/src/VowelOrConsonant.java
+++ VowelOrConsonant/src/VowelOrConsonant.java
@@ -10,18 +10,18 @@ class VowelOrConsonant {
 	
 	public static void main(String[] args) {
 		
-    	Scanner getInp = new Scanner(System.in);
-    	System.out.println("Whether enter character is an vowel or consonant program? by SM Satheesh");
-    	char ch = getInp.next().charAt(0);
-    	
-    	List<Character> chArrList = new ArrayList<Character>(Arrays.asList('A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u'));
-    	
-    	int result = checkIfIsAlphabet(ch, chArrList); // static return type method call
-    	if(result > -1)
-    		System.out.println("Entered character " + ch + " is an vowel");
-    	else
-    		System.out.println("Entered character " + ch + " is an consonant"); 
-    	
-    	getInp.close();
-    }
+	    	Scanner getInp = new Scanner(System.in);
+	    	System.out.println("Whether enter character is an vowel or consonant program? by SM Satheesh");
+	    	char ch = getInp.next().charAt(0);
+	    	
+	    	List<Character> chArrList = new ArrayList<Character>(Arrays.asList('A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u'));
+	    	
+	    	int result = checkIfIsAlphabet(ch, chArrList); // static return type method call
+	    	if(result > -1)
+	    		System.out.println("Entered character " + ch + " is an vowel");
+	    	else
+	    		System.out.println("Entered character " + ch + " is an consonant"); 
+	    	
+	    	getInp.close();
+	}
 }
