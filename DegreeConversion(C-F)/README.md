ea0356fe9cf034f103b8a8ae2bf73441bacc835a
SATHEESH <smsatheesh2000@gmail.com>
2023-10-17 21:59:32 +0530

    Degree conversion program of Celsius to Fahrenheit and vice versa committed to the community

---
+++ DegreeConversion(C-F)/README.md
@@ -0,0 +1,6 @@
+# Degree Conversion? (Java version)
+* Get degree value and option from user <br/>
+* Program determines degree conversion and displays value <br />
+* This program also demonstrates the usages of function with arguments, conditional statements, usages of Scanner class <br />
+
+### Contributed by [Sm Satheesh](https://github.com/smsatheesh)
---
+++ DegreeConversion(C-F)/src/DegreeConversion.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class DegreeConversion {
+	
+	static void performPowerOperation(int deg, int opt) {
+		
+		int resultantDeg;
+		switch(opt) {
+			case 1: 
+				resultantDeg = (deg * 9 / 5) + 32;
+				break;
+			case 2:
+				resultantDeg = (deg - 32) * 5 / 9;
+				break;
+			default:
+				System.out.println("Enter correct option !!");
+				return;
+		}
+		System.out.println("Resultant degree is " + resultantDeg);
+	}
+	
+	public static void main(String[] args) {
+		
+    	Scanner getInp = new Scanner(System.in);
+    	System.out.println("Power of a number program? by SM Satheesh");
+    	System.out.println("Enter option ?");
+    	System.out.println("\n1. Celsius to Farenheit\n2. Farenheit to Celsisus");
+    	int option = getInp.nextInt();
+    	System.out.println("Enter degree ?");
+    	int deg = getInp.nextInt();
+    	
+    	performPowerOperation(deg, option);
+    	
+    	getInp.close();
+    }
+}
