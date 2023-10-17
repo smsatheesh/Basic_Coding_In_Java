9542d8bb1ddd7556b15dc5d0b9ddf4902241f91e
SATHEESH <smsatheesh2000@gmail.com>
2023-10-17 23:04:10 +0530

    Count of digits of a numbers added to the community

---
+++ CountOfDigitsOfANumber/README.md
@@ -0,0 +1,6 @@
+# Print count of digits of a number? (Java version)
+* Get number or value from user <br/>
+* Program prints respective count of its digits <br />
+* This program also demonstrates the usages of function with arguments, looping statements, usage of Scanner class <br />
+
+### Contributed by [Sm Satheesh](https://github.com/smsatheesh)
---
+++ CountOfDigitsOfANumber/src/CountOfDigits.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class CountOfDigits {
+	
+	static void printCountOfDigits(int num) {
+		
+		int count = 0;
+		while(num != 0) {
+			count++;
+			num /= 10;
+		}
+		
+		System.out.println("Total digit count of a number is " + count);
+	}
+	
+	public static void main(String[] args) {
+		
+    	Scanner getInp = new Scanner(System.in);
+    	System.out.println("Count of digits of a number (ASC || DESC) program? by SM Satheesh");
+    	System.out.println("Enter number ?");
+    	int num = getInp.nextInt();
+    	
+    	printCountOfDigits(num);
+    	
+    	getInp.close();
+    }
+}
