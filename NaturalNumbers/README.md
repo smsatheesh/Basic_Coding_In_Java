9f8bad305ccd8bad8791de3edc2df86324888b31
SATHEESH <smsatheesh2000@gmail.com>
2023-10-17 22:51:12 +0530

    Natural numbers added to the community

---
+++ NaturalNumbers/README.md
@@ -0,0 +1,7 @@
+# Print natural numbers in asc or des? (Java version)
+* Get number or value from user <br/>
+* Program prints respective natural numbers within the range <br />
+* This program also demonstrates the usages of function with arguments, looping statements, usages of Scanner class <br />
+
+### Contributed by [Sm Satheesh](https://github.com/smsatheesh)
+### Contributed by [Sm Satheesh](https://github.com/smsatheesh)
---
+++ NaturalNumbers/src/NaturalNumbers.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class NaturalNumbers {
+
+	static void printNaturalNumbers(int start, int end) {
+		
+		if(start > end) {
+			
+			for(int loop = start; loop >= end; loop--)
+				System.out.print(loop + " ");
+		} else if(start < end) {
+			
+			for(int loop = start; loop <= end; loop++)
+				System.out.print(loop + " ");
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+    	Scanner getInp = new Scanner(System.in);
+    	System.out.println("Print 'n' natural numbers (ASC || DESC) program? by SM Satheesh");
+    	System.out.println("Enter start range ?");
+    	int start = getInp.nextInt();
+    	System.out.println("Enter end range");
+    	int end = getInp.nextInt();
+    	
+    	printNaturalNumbers(start, end);
+    	
+    	getInp.close();
+    }
+}
