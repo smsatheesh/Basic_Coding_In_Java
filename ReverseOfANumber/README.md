033b4a043515cdb0d268cc50a085243759e4a8b5
SATHEESH <smsatheesh2000@gmail.com>
2023-10-19 23:03:25 +0530

    Reverse of a number added to the community

---
+++ ReverseOfANumber/README.md
@@ -0,0 +1,6 @@
+# REverse of digits of a number? (Java version)
+* Get number or value from user <br/>
+* Program prints respective reverse format of a number <br />
+* This program also demonstrates the usages of function with arguments, looping statements, usages of Scanner, StringBuilder class <br />
+
+### Contributed by [Sm Satheesh](https://github.com/smsatheesh)
---
+++ ReverseOfANumber/src/ReverseOfANumber.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class ReverseOfANumber{
+	
+	static void printReverseOfDigitsUsingString(int num) {
+		
+		String str = String.valueOf(num);
+		StringBuilder result = new StringBuilder(str);
+		result.reverse();
+		Integer res = Integer.valueOf(result.toString());
+		
+		System.out.println("Reverse number using string logic is " + res);
+	}
+	
+	static void printReverseOfDigitsUsingMath(int num) {
+	
+		int result = 0;
+		while(num != 0) {
+			
+			result = (result * 10) + (num % 10);
+			num /= 10;
+		}
+		
+		System.out.println("Reverse number using math logic is " + result);
+	}
+	
+	public static void main(String[] args) {
+		
+    	Scanner getInp = new Scanner(System.in);
+    	System.out.println("Reverse of digits of a number program? by SM Satheesh");
+    	System.out.println("Enter number ?");
+    	int num = getInp.nextInt();
+    	
+    	printReverseOfDigitsUsingString(num); // using String concepts
+    	printReverseOfDigitsUsingMath(num);   // using math concepts
+    	
+    	getInp.close();
+    }
+}
