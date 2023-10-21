f1c21382c45f424b83063a852e9efd0ddf2728eb
SATHEESH <smsatheesh2000@gmail.com>
2023-10-21 08:40:06 +0530

    Even and odd elements count in array added to the community

---
+++ EvenAndOddCountInArray/README.md
@@ -0,0 +1,6 @@
+# Even and odd elements count program ? (Java version)
+* Get number or value from user and get array values <br/>
+* Program prints respective odd and even count of array <br />
+* This program also demonstrates the usages of function with arguments, looping statements, usages of Scanner <br />
+
+### Contributed by [Sm Satheesh](https://github.com/smsatheesh)
---
+++ EvenAndOddCountInArray/src/EvenAndOddCountInArray.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class EvenAndOddCountInArray {
+
+	static void printEvenAndOddCounts(int[] arr, int len) {
+	
+		int evenCount = 0, oddCount = 0;
+		for(int loopItr = 0; loopItr < len; loopItr++) {
+			
+			if(arr[loopItr] % 2 == 0)
+				evenCount++;
+			else if(arr[loopItr] % 2 != 0)
+				oddCount++;
+		}
+		
+		System.out.println("Event elements count " + evenCount);
+		System.out.println("Odd elements count " + oddCount);
+	}
+	
+	public static void main(String[] args) {
+		
+    	Scanner getInp = new Scanner(System.in);
+    	System.out.println("Enter size of the element ? ");
+    	int size = getInp.nextInt();
+    	System.out.println("Enter elements ... ");
+    	int[] arr = new int[size];
+    	for(int loopItr = 0; loopItr < size; loopItr++)
+    		arr[loopItr] = getInp.nextInt();
+    	
+    	printEvenAndOddCounts(arr, size);
+    	
+    	getInp.close();
+    }
+}
