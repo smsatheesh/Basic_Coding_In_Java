276a41ac8e584dd2f721586faba1aea68b182d1e
SATHEESH <smsatheesh2000@gmail.com>
2023-10-21 09:33:03 +0530

    Added program to the community whether entered number is a palindrome

---
+++ NumberIsPalindrome/README.md
@@ -0,0 +1,6 @@
+# Is number a palindrome ? (Java version)
+* Get number or value from user <br/>
+* Program prints whether entered number is a palindrome <br />
+* This program also demonstrates the usages of function with arguments, looping statements, usages of Scanner, StringBuilder class <br />
+
+### Contributed by [Sm Satheesh](https://github.com/smsatheesh)
---
+++ NumberIsPalindrome/src/NumberIsPalindrome.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class NumberIsPalindrome {
+	
+	static void checkPalindromeUsingStringConc(int num) {
+		
+		String strOfNum = String.valueOf(num);
+		StringBuilder strBuild = new StringBuilder(strOfNum);
+		
+		if((String.valueOf(num)).equals(strBuild.reverse().toString()))
+			System.out.println(num + " is in palindrome");
+		else
+			System.out.println(num +  " is not a palindrome");
+	}
+
+	static void checkPalindromeUsingMathLogic(int num) {
+		
+		int temp = num;
+		int rev = 0;
+		while(num != 0) {
+			rev = (rev * 10) + (num % 10);
+			num /= 10;
+		}
+		
+		if(temp == rev)
+			System.out.println(temp + " is in palindrome");
+		else
+			System.out.println(temp + " is not a palindrome");
+	}
+	
+	public static void main(String[] args) {
+		
+		Scanner getInp = new Scanner(System.in);
+		System.out.println("Enter number ? ");
+		int num = getInp.nextInt();
+		
+		checkPalindromeUsingStringConc(num); // using String concept
+		checkPalindromeUsingMathLogic(num);
+	}
+}
