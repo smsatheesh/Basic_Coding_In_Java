f6de13df39cda9c983c2580d9bd3d19ff27d203c
SATHEESH <smsatheesh2000@gmail.com>
2023-10-21 18:04:55 +0530

    Fibonacci series program added to the community

---
+++ FibonacciSeries/README.md
@@ -0,0 +1,6 @@
+# Fibonacci Series ? (Java version)
+* Get number or value from user <br/>
+* Program prints Fibonacci series with according to the range of user input <br />
+* This program also demonstrates the usages of function with arguments, looping statements, usages of Scanner class, recursive code implementation <br />
+
+### Contributed by [Sm Satheesh](https://github.com/smsatheesh)
---
+++ FibonacciSeries/src/FibonacciSeries.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+class FibonacciSeries {
+	
+	static int n1 = 0, n2 = 1;
+	static void printFibonacciSeriresUsingIterative(int num) {
+	
+		if(num >= 2) {
+
+			int n1 = 0, n2 = 1;
+			System.out.print(n1 + " " + n2 + " ");
+			for(int i = 0; i < num - 2; i++) {
+				
+				int n3 = n1 + n2;
+				n1 = n2;
+				n2 = n3;
+				System.out.print(n3 + " ");
+			}
+		} else if(num == 1)
+			System.out.print(0);
+	}
+	
+	static void printFibonacciSeriesUsingRecursion(int count) {
+		
+		if(count > 0) {
+			int n3 = n2 + n1;
+			System.out.print(n3 + " ");
+			n1 = n2;
+			n2 = n3;
+			printFibonacciSeriesUsingRecursion(count-1);
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+    	Scanner getInp = new Scanner(System.in);
+    	System.out.println("Fibonacci Series");
+    	System.out.println("Enter count to print ? ");
+    	int num = getInp.nextInt();
+    	
+    	System.out.println("Using Iteration ::: ");
+    	printFibonacciSeriresUsingIterative(num); // Iterative process
+    	System.out.println("\nUsing Recursion ::: ");
+    	System.out.print(n1+ " " + n2 + " ");
+    	printFibonacciSeriesUsingRecursion(num - 2);  // Recursive process
+    	
+    	getInp.close();
+    }
+}
