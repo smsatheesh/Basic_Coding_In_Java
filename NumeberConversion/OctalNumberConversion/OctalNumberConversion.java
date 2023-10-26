cafc545f733216ed285e236a0e51c1f78fccec86
SATHEESH <smsatheesh2000@gmail.com>
2023-10-26 21:46:09 +0530

    Octal number conversion added to the community

---
+++ NumeberConversion/OctalNumberConversion/OctalNumberConversion.java
@@ -0,0 +1,96 @@
+import java.util.*;
+
+class OctalNumberConversion {
+	
+	static void printOctalToBinaryUsingInBuildMethod(int octal) {
+		
+		int octalValue = intOctalToDecimalUsingInBuildMethod(octal);
+		System.out.println(Integer.toBinaryString(octalValue));
+	}
+	
+	static void printOctalToBinaryUsingMathLogic(int octal) {
+		
+		int decimalValue = intOctalToDecimalUsingInBuildMethod(octal);
+		int binaryValue = 0, i = 1;
+		while(decimalValue != 0) {
+			
+			binaryValue += (decimalValue % 2) * i;
+			decimalValue /= 2;
+			i *= 10;
+		}
+		System.out.print(binaryValue);
+		System.out.println();
+	}
+	
+	static int intOctalToDecimalUsingInBuildMethod(int octal) {
+		
+		return Integer.parseInt(String.valueOf(octal), 8);	
+	}
+	
+	static void printOctalToDecimalUsingMathLogic(int octal) {
+		
+		int decimalValue = 0, i = 0, rem = 0;
+		
+		while(octal != 0) {
+			
+			rem = octal % 10;
+			decimalValue += rem * Math.pow(8, i);
+			++i;
+			octal /= 10;
+		}
+		
+		System.out.print(decimalValue);
+	}
+	
+	static void printOctalToHexaUsingInBuildMethod(int octal) {
+		
+		int decimalValue = intOctalToDecimalUsingInBuildMethod(octal);
+		System.out.println(Integer.toHexString(decimalValue));
+	}
+	
+	static void printOctalToHexUsingMathLogic(int octal) {
+		
+		String hexValue = "";
+		char[] hexChars = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+		int decimalValue = intOctalToDecimalUsingInBuildMethod(octal);
+		
+		while(decimalValue != 0) {
+			
+			int rem = decimalValue % 16;
+			hexValue = hexChars[rem++] + hexValue;
+			decimalValue /= 16;
+		}
+		
+		System.out.println(hexValue);
+	}
+
+	public static void main(String[] args) {
+		
+		Scanner getInp = new Scanner(System.in);
+		System.out.print("Enter octal number ::: ");
+		int num = getInp.nextInt();
+		
+		// OCTAL to BIN conversion
+		System.out.println("\n--- BINARY CONVERSION ---");
+		System.out.print("In-build methods -> ");
+		printOctalToBinaryUsingInBuildMethod(num);
+		System.out.print("Using Math logic -> ");
+		printOctalToBinaryUsingMathLogic(num);
+		
+		// OCTAL to DECIMAL conversion
+		System.out.println("\n\n--- OCTAL CONVERSION ---");
+		System.out.print("In-build methods -> ");
+		int resP = intOctalToDecimalUsingInBuildMethod(num);
+		System.out.println(resP);
+		System.out.print("Using Math logic -> ");
+		printOctalToDecimalUsingMathLogic(num);
+
+		// OCTAL to HEX conversion
+		System.out.println("\n\n--- HEXA CONVERSION ---");
+		System.out.print("In-build methods -> ");
+		printOctalToHexaUsingInBuildMethod(num);
+		System.out.print("Using Math logic -> ");
+		printOctalToHexUsingMathLogic(num);
+		
+	}
+}
---
+++ NumeberConversion/OctalNumberConversion/README.md
@@ -0,0 +1,6 @@
+# Octal to binary, decimal, hexa conversion ? (Java version)
+* Get number or value from user <br/>
+* Program prints respective other format of a number <br />
+* This program also demonstrates the usages of function with arguments, looping statements, usages of Scanner class <br />
+
+### Contributed by [Sm Satheesh](https://github.com/smsatheesh)
