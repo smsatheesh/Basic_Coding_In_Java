fe2967fd350e11ac60bc5e3aed13297b47a92406
smsatheesh <smsatheesh2000@gmail.com>
2023-11-05 15:41:23 +0530

    Recursive Fibonacci added to the community

---
+++ Recursion/RecursiveFibonacciN_number/README.md
@@ -0,0 +1,5 @@
+# Recursive Fibonacci upto 'N'th number ? `( JAVA version )`
+
+* Get the last or high range value from the user.
+* The program performs recursive fibonacci and display results to the user.
+* This program uniquely demonstrates the `usages of RECURSION`.
---
+++ Recursion/RecursiveFibonacciN_number/src/RecursiveFibonacciN_number.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class RecursiveFibonacciN_number {
+	
+	static int fiboN( int n ) {
+		
+		if( n == 0 )
+			return 0;
+		else if( n == 1 )
+			return 1;
+		
+		return fiboN( n - 1 ) + fiboN( n - 2 );
+	}
+	
+	public static void main(String[] args) {
+
+		Scanner getInp = new Scanner( System.in );
+		System.out.println( "Enter position of number to fibonacci : " );
+		int position = getInp.nextInt();
+		System.out.println( fiboN( position ) );
+	}
+}
