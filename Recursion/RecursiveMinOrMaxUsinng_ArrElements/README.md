0db4c77df0930404a82d3ed5e588c5d2d5611a69
smsatheesh <smsatheesh2000@gmail.com>
2023-11-10 20:11:42 +0530

    array recursive min and max element in array display to the user added to the community

---
+++ Recursion/RecursiveMinOrMaxUsinng_ArrElements/README.md
@@ -0,0 +1,5 @@
+# Recursive Array Min and Max Elements ? `( JAVA version )`
+
+* Get the array value from the user.
+* The program performs recursive computation min and max thereby, display results to the user.
+* This program uniquely demonstrates the `usages of RECURSION`.
---
+++ Recursion/RecursiveMinOrMaxUsinng_ArrElements/src/RecursiveMinOrMaxUsinng_ArrElements.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class RecursiveMinOrMaxUsinng_ArrElements {
+
+	static int printArrayRecursiveMaxOrMinElement( int arr[], int sz, String type ) {
+		
+		if( sz == 0 )
+			return arr[0];
+		else {
+			if( type.equals( "MAX" ) )
+				return Math.max( arr[sz - 1], printArrayRecursiveMaxOrMinElement( arr, sz - 1, "MAX" ));
+			else if( type.equals( "MIN" ) )
+				return Math.min( arr[sz-1] , printArrayRecursiveMaxOrMinElement( arr, sz - 1, "MIN" ));
+		}
+		
+		return -1;
+	}
+	
+	public static void main(String[] args) {
+		
+		Scanner getInp = new Scanner(System.in);
+		System.out.println( "Display Array Elements Using Recursion" );
+		System.out.println( "Enter size : " );
+		int size = getInp.nextInt();
+		
+		int[] arr = new int[size];
+		System.out.println( "Enter elements : " );
+		for( int loopVar = 0; loopVar < size; loopVar++ )
+			arr[loopVar] = getInp.nextInt();
+		
+		int maxElement = printArrayRecursiveMaxOrMinElement( arr, size, "MAX" );
+		System.out.println( maxElement + " is the MAX element in array" );
+		
+		int minElement = printArrayRecursiveMaxOrMinElement( arr, size, "MIN" );
+		System.out.println( minElement + " is the MIN element in array" );
+	}
+}
