7b3f571583459cc3608d3fee6a1a82b03c868de8
smsatheesh <smsatheesh2000@gmail.com>
2023-11-22 04:08:47 +0530

    Added hours, min, sec conversion program to the community

---
+++ HoursMinSecConversion/README.md
@@ -0,0 +1,7 @@
+# Hours, Minutes and Seconds Conversion ? `( JAVA Version )`
+
+* Program pre-defines the input.
+* The program has an mathematical approach for the conversion of hous, mintues and seconds to the higher order format( hours -> minutes -> seconds).
+* The program used the concepts of methods overloading, Strings and bottom-up program approach, functions usages.
+
+### Contributed by [Sm Satheesh](https://github.com/smsatheesh) 
---
+++ HoursMinSecConversion/src/HoursMinSecConversion.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+class HoursMinSecConversion {
+
+	private static final String INVALID_MSG = "Invalid value !";
+	
+	private static String getDurationString( long sec ) {
+		
+		if( sec < 0 ) 
+			return INVALID_MSG;
+		else {
+			
+			return getDurationString( sec/60, sec%60 );
+		}
+	}
+	
+	private static String getDurationString( long min, long sec ) {
+		
+		if( min < 0 )
+			return INVALID_MSG;
+		else {
+			
+			String hrsString = "00", minString = "00", secString = "00";
+			long hrs = min / 60;
+			min = min % 60;
+			min += sec /60;
+			sec %= 60;
+			
+			hrsString = String.valueOf( hrs );
+			minString = String.valueOf( min );
+			secString = String.valueOf( sec );
+			if( hrs < 10 )
+				hrsString = "0" + hrsString;
+			
+			if( min < 10 )
+				minString = "0" + minString;
+			
+			if( sec < 10 )
+				secString = "0" + secString;
+			
+			return ( hrsString + " HH's " + minString + " MM's " + secString + " SS's" );
+		}
+	}
+
+	public static void main(String[] args) {
+		
+		System.out.println( getDurationString( 65, 95 ) );
+		System.out.println( getDurationString( 95 ) );
+	}
+}
