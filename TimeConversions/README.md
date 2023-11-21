e904b4589a0700ebe200db6ed22f2aafe6695651
smsatheesh <smsatheesh2000@gmail.com>
2023-11-22 04:53:18 +0530

    Program modifications

---
+++ TimeConversions/README.md
@@ -0,0 +1,7 @@
+# Time Conversion ? `( JAVA Version )`
+
+* Get value of minutes and seconds from the user.
+* The program has an mathematical approach for the conversion of years, months, dayas, hours, mintues and seconds to the higher order format( year -> months-> days-> hours -> minutes -> seconds).
+* The program used the concepts of methods overloading, Strings and bottom-up program approach, functions usages.
+
+### Contributed by [Sm Satheesh](https://github.com/smsatheesh) 
--- TimeConversions/src/TimeConversions.java
+++ TimeConversions/src/TimeConversions.java
@@ -1,17 +1,38 @@
 import java.util.*;
 
-class HoursMinSecConversion {
+class TimeConversions {
 
-	private static final String INVALID_MSG = "Invalid value !";
-	
-	private static String getDurationString( long sec ) {
+	private static String getDurationString( long hrs, long min, long sec, String hrStr, String minStr, String secStr ) {
 		
-		if( sec < 0 ) 
-			return INVALID_MSG;
-		else {
-			
-			return getDurationString( sec/60, sec%60 );
-		}
+		String yrString = "00", monthString = "00", dayString = "00";
+		long yr = 0, mon = 0, days = 0;
+		
+		days = hrs / 24;
+		if( days >= 30 )
+			mon = days / 30;
+		if( mon >= 12 )
+			yr = mon / 12;
+		
+		hrs = hrs % 24;
+		days = days % 30;
+		mon = mon % 12;
+		
+		dayString = String.valueOf( days );
+		monthString = String.valueOf( mon );
+		yrString = String.valueOf( mon );
+		
+		if( days < 0 )
+			dayString = "0" + dayString;
+		
+		if( mon < 0 )
+			monthString = "0" + monthString;
+		
+		if( yr < 0 )
+			yrString = "0" + yrString;
+		
+		hrStr = ( hrs != 0 ) ? String.valueOf( hrs ): "00";
+		
+		return ( yrString + " Year's " + monthString + " Month's " + dayString  + " Day's " + hrStr + " HH's " + minStr + " MM's " + secStr + " SS's" );
 	}
 	
 	private static String getDurationString( long min, long sec ) {
@@ -38,19 +59,33 @@ class HoursMinSecConversion {
 			if( sec < 10 )
 				secString = "0" + secString;
 			
+			if( hrs >= 24 )
+				return getDurationString( hrs, min, sec, hrsString, minString, secString );
+			
 			if( min >= 60 )
 				return getDurationString( min, sec );
 			
 			return ( hrsString + " HH's " + minString + " MM's " + secString + " SS's" );
 		}
 	}
-
+	
+	private static String getDurationString( long sec ) {
+		
+		if( sec < 0 ) 
+			return INVALID_MSG;
+		else {
+			
+			return getDurationString( sec/60, sec%60 );
+		}
+	}
+	
+	
 	public static void main(String[] args) {
 		
 		long min, sec;
 		Scanner getInp = new Scanner( System.in );
 	
-		System.out.println( "Hour's Minute's And Seconds Conversion ? " );
+		System.out.println( "Time Conversion ? " );
 		System.out.print( "\nEnter Minutes :: " );
 		min = getInp.nextLong();
 		System.out.print( "Enter Seconds :: " );
