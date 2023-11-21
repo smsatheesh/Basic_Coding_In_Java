5fc6d255b2a725d065d95356bc55fffde7c24acf
smsatheesh <smsatheesh2000@gmail.com>
2023-11-21 09:19:01 +0530

    Added string characteristics to the community

---
+++ String/StringCharactersitics/src/StringCharactersitics.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class StringCharactersitics {
+	
+	static boolean checkStringHasAllUpperCase( String str ) {
+		
+		char[] chArray = str.toCharArray();
+		
+		for( char chLit: chArray ) {
+			
+			if( Character.isLowerCase( chLit ) )
+				return false;	// returns false if String has character any in LOWER CASE
+		}
+		
+		return true;			// returns true if String has character all in UPPER CASE
+	}
+	
+	public static void main(String[] args) {
+		
+		System.out.println( "String Checks, If do have Upper (or) Lower Case ?" );
+		Scanner getInp = new Scanner(System.in);
+		
+		System.out.print( "Enter string :: " );		
+		String str = getInp.nextLine();
+	
+		System.out.println( checkStringHasAllUpperCase( str ) );
+	}
+}
