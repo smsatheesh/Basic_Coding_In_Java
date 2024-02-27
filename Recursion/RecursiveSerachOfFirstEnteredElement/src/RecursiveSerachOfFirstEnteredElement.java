7e8db8bb3f2e889b279fbbb7944a03b7b4018bc2
smsatheesh <smsatheesh2000@gmail.com>
2024-02-27 19:09:09 +0530

    Recursive number search of first occurrence have been added to the community

---
+++ Recursion/RecursiveSerachOfFirstEnteredElement/src/RecursiveSerachOfFirstEnteredElement.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class RecursiveSerachOfFirstEnteredElement {
+
+	static int checkAndReturnIndex( int num, int[] arr, int idx ) {
+		
+		if( idx == arr.length )
+			return -1;
+		else if( num == arr[idx] )
+			return ( idx + 1 );
+		else
+			return checkAndReturnIndex( num, arr, idx+1 );
+	}
+	
+	public static void main (String[] args) {
+    	
+		Scanner getInp = new Scanner( System.in );
+		System.out.println( "Enter number to check : " );
+		int num = getInp.nextInt();
+		System.out.println( "Enter size of an array : " );
+		int sz = getInp.nextInt();
+		int[] arr = new int[ sz ];
+		System.out.println( "Enter elements : " );
+		for( int i = 0; i < num; i++ )
+			arr[i] = getInp.nextInt();
+		
+		int result = checkAndReturnIndex( num, arr, 0);
+		if( result != -1 )
+			System.out.println( "Element present, at " + result );
+		else
+			System.out.println( "Element not present" );
+    }
+}
