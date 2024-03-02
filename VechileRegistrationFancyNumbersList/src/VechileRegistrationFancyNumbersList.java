27729dcd8813a63df08e2fe415ca60e13a926057
smsatheesh <smsatheesh2000@gmail.com>
2024-03-02 09:31:44 +0530

    Fancy number list for vehicle registration have been added to the community

---
+++ VechileRegistrationFancyNumbersList/src/VechileRegistrationFancyNumbersList.java
@@ -0,0 +1,43 @@
+public class VechileRegistrationFancyNumbersList {
+	
+	static boolean getSum7Check( int num ) {
+		
+		int val = 0;
+		
+		while( num != 0 ) {
+			val += num % 10;
+			num /= 10;
+		}
+		if( val <= 10 )
+			return ( val == 7 );
+		else
+			return getSum7Check( val );
+	}
+	
+	static boolean checkDigitsOfOddNumberSame( int num ) {
+		
+		char[] ch = String.valueOf( num ).toCharArray();
+		if( ch[ 1 ] == ch[ 3 ] )
+			return true;
+		else
+			return false;
+	}
+	
+	static boolean checkNumbersAreInUpFace( int num ) {
+		
+		char[] ch = String.valueOf( num ).toCharArray();
+		if( ch[ 0 ] <= ch[ 2 ] )
+			return true;
+		else
+			return false;
+	}
+	
+	public static void main( String[] args ) {
+		
+		for( int i = 1000; i < 10000; ++i ) {
+			if( checkDigitsOfOddNumberSame( i ) && getSum7Check( i ) && checkNumbersAreInUpFace( i ) ) {
+				System.out.println( i );
+			}
+		}
+	}
+}
